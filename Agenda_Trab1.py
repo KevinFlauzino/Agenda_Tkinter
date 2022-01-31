c5c4f4d7e45d6896e2e343977dde343eb71603d8
KevinFlauzino <97861845+KevinFlauzino@users.noreply.github.com>
2022-01-31 16:17:00 -0300

    Add files via upload

--- Agenda_Trab1.py
+++ Agenda_Trab1.py
@@ -1,55 +1,14 @@
-from distutils import command
+#from distutils import command
 from tkinter import *
-from tkinter import simpledialog
+#from tkinter import simpledialog
 
 class Agenda():
+    def __init__(self, nome, numero, email):
+        self.nome = nome        
+        self.numero = numero
+        self.email = email
 
-    def Inserir(): #Insesir contato        
-        InserirMenu = Tk()
-        InserirMenu.title("Inserindo novo contato")
-        InserirMenu.geometry("500x400+500+100")
-        InserirMenu.resizable(False, False)
-
-        Label(InserirMenu, text="Digite o nome: ").grid(row=0, sticky=W)
-        Label(InserirMenu, text="Digite o número: ").grid(row=1, sticky=W)
-        Label(InserirMenu, text="Digite o email: ").grid(row=2, sticky=W)
-
-        def Inserir2():
-            Nomex = Nome.get()              
-
-            print("Nome: ",Nomex, "\nNumero: ",Numerox, "\nEmail: ", Emailx) 
-
-            #contatos = open('Contatos.txt', 'a')       
-            #contatos.write("{}-{}-{}\n".format(Nomex, Numerox, Emailx)) 
-            #contatos.close()
-
-        Nome = Entry(InserirMenu, width=25).grid(row=0, column=1)
-        Numero = Entry(InserirMenu, width=25).grid(row=1, column=1)
-        Email = Entry(InserirMenu, width=25).grid(row=2, column=1)
-
-
-
-        ButtonOk = Button(  InserirMenu,
-                            text="Adicionar",
-                            command = Inserir2
-                            ).grid(row=4, column=0)   
-    
-    def Remover():
-        pass
-
-    def Buscar():
-        pass
-
-    def Editar():
-        pass
-
-
-contatos = Agenda()
-
-class Menu: #Criando menu para poder usar como loop
-    def Iniciar_menu(self): 
-
-
+    def menu():
         AgendaMenu = Tk()
         AgendaMenu.title("Sua Agenda :)")
 
@@ -59,7 +18,7 @@ class Menu: #Criando menu para poder usar como loop
         AgendaMenu["bg"] = "#00997a"      
 
         buttonColors = "#00cca3"
-        buttonsLargura = 20        
+        buttonsLargura = 20 
 
         MenuTitulo = Label( AgendaMenu,
                             text="Menu de opções",                  #Título
@@ -75,28 +34,28 @@ class Menu: #Criando menu para poder usar como loop
                             bg = buttonColors,
                             font = "ArialBlack 15 bold",
                             width = buttonsLargura,                 #Largura do botão
-                            command = Agenda.Inserir) 
+                            command = agenda1.inserir) 
 
         RemoverB = Button(  AgendaMenu,
                             text="Remover contato",
                             bg = buttonColors,
                             font = "ArialBlack 15 bold",
                             width = buttonsLargura,
-                            command = Agenda.Remover)
+                            command = agenda1.remover)
                                     
         BuscarB = Button(   AgendaMenu,
                             text="Buscar contato",
                             bg = buttonColors,
                             font = "ArialBlack 15 bold",
                             width = buttonsLargura,
-                            command = Agenda.Buscar)
+                            command = agenda1.buscar)
 
         EditarB = Button(   AgendaMenu,
                             text="Editar contato",
                             bg = buttonColors,
                             font = "ArialBlack 15 bold",
                             width = buttonsLargura,
-                            command = Agenda.Editar)
+                            command = agenda1.editar)
 
         FinalizarB = Button(AgendaMenu,
                             text="Finalizar Agenda",
@@ -110,9 +69,61 @@ class Menu: #Criando menu para poder usar como loop
         EditarB.pack()
         FinalizarB.pack()      
 
-        AgendaMenu.mainloop()       
-           
-    
-menu = Menu() #setando
-escolhaX = 0 
-menu.Iniciar_menu()
+        AgendaMenu.mainloop()
+
+    def inserir_aux(self):
+        
+        self.nome.get() 
+        self.numero.get()
+        self.email.get()  
+
+        print("Dentro da função Nome:",self.nome)
+        print("Dentro da função Numero:",self.numero)
+        print("Dentro da função Email:",self.email)     
+
+        agenda1 = open("Contatos.txt", "a")
+        agenda1.write("{}-{}-{}\n".format(self.nome, self.numero, self.email))
+
+    def inserir(self): #Insesir contato        
+        InserirMenu = Tk()
+        InserirMenu.title("Inserindo novo contato")
+        InserirMenu.geometry("500x400+500+100")
+        InserirMenu.resizable(False, False)
+
+        Label(InserirMenu, text="Digite o nome: ").grid(row=0, sticky=W)
+        Label(InserirMenu, text="Digite o número: ").grid(row=1, sticky=W)
+        Label(InserirMenu, text="Digite o email: ").grid(row=2, sticky=W)
+        
+        self.nome = Entry(InserirMenu, width=25)
+        self.nome.grid(row=0, column=1)                    
+        print(self.nome)
+
+        self.numero = Entry(InserirMenu, width=25)
+        self.numero.grid(row=1, column=1)
+        print(self.numero)
+
+        self.email = Entry(InserirMenu, width=25)
+        self.email.grid(row=2, column=1) 
+        print(self.email)
+
+        button_inserir = Button(  InserirMenu,
+                            text="Adicionar",
+                            command =lambda: Agenda.inserir_aux(self)
+                            )
+        button_inserir.grid(row=4, column=0)        
+
+    def remover():
+        pass
+
+    def buscar():
+        pass
+
+    def editar():
+        pass
+
+#nome = ""
+#numero = ""
+#email = ""
+
+agenda1 = Agenda()
+Agenda.menu()  
